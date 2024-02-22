e96a261a65729bad73a622dff9b01f965d67a80f
Fred <scanafei@gmail.com>
2024-02-22 13:05:49 +0100

    loading fils

--- ObjectDetection/run.sh
+++ ObjectDetection/run.sh
@@ -10,4 +10,4 @@ cd faster_rcnn/lib
 cd ../..
 
 # Execute the training/validation script with specified parameters
-/bin/python3 faster_rcnn/trainval_net.py --dataset voc --data_root /data1/fontana/BiDet/data/ --basenet /data1/fontana/CycleBNN/pruning/logs/imagenet_resnet_18/dulcet-glitter-408_checkpoint_8947500_60.45.pth.tar
+/bin/python3 ../ObjectDetection/faster_rcnn/trainval_net.py --dataset voc --data_root ../ObjectDetection/data/ --basenet "path to weights"
